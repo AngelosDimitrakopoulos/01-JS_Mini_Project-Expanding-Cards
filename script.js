7d099c1665398cdeae9895d1dc42c4d553606baf
Angelos Dimitrakopoulos <angdimitrakopoulos@gmail.com>
2024-02-23 11:12:01 +0200

    final commit

--- script.js
+++ script.js
@@ -4,19 +4,14 @@
 const cards = document.querySelectorAll(".panel");
 
 cards.forEach((card) => {
-  // Select the h3 of the current panel
-  const heading = card.querySelector("h3");
-  // Add an event listener to the current
-  card.addEventListener("click", (e) => {
-    // Close open card before displaying the clicked one
-    cards.forEach((panel) => {
-      panel.classList.remove("active");
-      const panelHeading = panel.querySelector("h3");
-      panelHeading.classList.add("hide");
-    });
-
-    // Display the clicked card
-    e.currentTarget.classList.add("active");
-    heading.classList.remove("hide");
+  card.addEventListener("click", () => {
+    removeActiveClasses();
+    card.classList.add("active");
   });
 });
+
+function removeActiveClasses() {
+  cards.forEach((card) => {
+    card.classList.remove("active");
+  });
+}
